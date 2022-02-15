18cba75a5176e81e84bb62a96725a3abafabb450
kangxiaoye <2198695788@qq.com>
2022-02-15 19:38:10 +0800

    add new file

---
+++ vue-evernote-client/src/components/Login.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+      <h1>{{msg}}</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Login",
+      data(){
+          return{
+            msg:"this is login"
+          }
+      }
+    }
+</script>
+
+<style scoped>
+h1 {
+  color: #42b983;
+}
+</style>
---
+++ vue-evernote-client/src/components/NotebookList.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+    export default {
+        name: "NotebookList"
+    }
+</script>
+
+<style scoped>
+
+</style>
