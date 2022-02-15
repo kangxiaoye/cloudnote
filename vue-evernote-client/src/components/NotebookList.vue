13b9f9ab2e9121ce16302416ecca52f941e4f211
kangxiaoye <2198695788@qq.com>
2022-02-15 20:17:14 +0800

    add new files

--- vue-evernote-client/src/components/NotebookList.vue
+++ vue-evernote-client/src/components/NotebookList.vue
@@ -1,13 +1,26 @@
 <template>
-
+  <div id="notebook-list">
+    <h1>{{msg}}</h1>
+    <ul>
+      <li><router-link to="/NoteDetail/1">笔记本1</router-link></li>
+      <li><router-link to="/NoteDetail/2">笔记本2</router-link></li>
+    </ul>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: "NotebookList"
+  export default {
+    name: 'Login',
+    data () {
+      return {
+        msg: '笔记本列表'
+      }
     }
+  }
 </script>
 
 <style scoped>
-
+  h1 {
+    color: red;
+  }
 </style>
--- vue-evernote-client/src/router/index.js
+++ vue-evernote-client/src/router/index.js
@@ -2,6 +2,9 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 import Login from "../components/Login";
+import NotebookList from "../components/NotebookList";
+import NoteDetail from "../components/NoteDetail";
+import TrashDetail from "../components/TrashDetail";
 Vue.use(Router)
 
 export default new Router({
@@ -15,6 +18,21 @@ export default new Router({
       path: '/Login',
       name: 'Login',
       component: Login
+    },
+    {
+      path: '/NotebookList',
+      name: 'NotebookList',
+      component: NotebookList
+    },
+    {
+      path: '/NoteDetail/:noteId',
+      name: 'NoteDetail',
+      component: NoteDetail
+    },
+    {
+      path: '/TrashDetail/:noteId',
+      name: 'TrashDetail',
+      component: TrashDetail
     }
   ]
 })
